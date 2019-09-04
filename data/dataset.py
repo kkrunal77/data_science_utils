0c8a52dfb3bae4f18599d83c2db5baeb832a2115
krunal <k.krunal77@gmail.com>
2019-09-04 21:26:35 +0530

    dataset

---
+++ data/dataset.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+# See available datasets
+# print(tfds.list_builders())
+import tensorflow_dataset as tfds
+
+"""https://www.tensorflow.org/datasets"""
+class datareader(object):
+    def __init__(self):
+        super(get_dataset, self).__init__()
+        tf.enable_eager_execution()
+
+    def getdata(self , data_set_name, batch_size=False, train_split=True):
+        super(get_dataset, self).__init__()
+        self.data_set_name = data_set_name
+        self.batch_size = batch_size
+        self.train_split = train_split
+        if self.batch_size and self.train_split:
+            """exp. [mnist, cifar10,...]"""
+            dataset = tfds.load(name=data_set_name.lower(), split=tfds.Split.TRAIN)
+            dataset = dataset.shuffle(1024).batch(self.batch_size).prefetch(tf.data.experimental.AUTOTUNE)
+            return  dataset
+        else:
+            dataset = tfds.load(name=data_set_name.lower(), split=tfds.Split.TRAIN)
+            return dataset
